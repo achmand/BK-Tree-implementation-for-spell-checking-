83fa5ab679b8da67f172840fe5c2fd9525acbc5b
achmand- Dylan Vassallo <dylan.vassallo@hotmail.com>
2017-04-14 02:30:56 +0200

    added a simple example. TODO -> add words/dictionary and save the graph, once its built.

--- BkTreeSpellChecker/BkTreeSpellChecker/Program.cs
+++ BkTreeSpellChecker/BkTreeSpellChecker/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace BkTreeSpellChecker
+﻿using System;
+using BkTreeSpellChecker.StringMetrics;
+
+namespace BkTreeSpellChecker
 {
     class Program
     {
         static void Main(string[] args)
         {
+            var bkTree = new BkTree.BkTree(new BkLevenshteinDistance());
+            bkTree.AddNode("book");
+            bkTree.AddNode("books");
+            bkTree.AddNode("boo");
+            bkTree.AddNode("boon");
+            bkTree.AddNode("cook");
+            bkTree.AddNode("cake");
+            bkTree.AddNode("cape");
+            bkTree.AddNode("cart");
+
+            var result = bkTree.SpellCheck("boa", 2);
+            Console.ReadLine();
         }
     }
 }
